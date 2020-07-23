caadd992372749aed2e7d64c83d70cb5cf7aeba2
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-22 21:07:51 -0300

    Function format

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { Container } from './styles';
 
-function Header() {
+const Header = () => {
   return (
     <Container>
       <a href='https://linkedin.com/in/lucasadornof'
       target='_blank' rel="noopener noreferrer"
       ><h1>@Note</h1></a>
-    </ Container>
+    </Container>
   );
 }
 
